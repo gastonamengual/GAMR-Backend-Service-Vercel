9cd1560691cb4e265d078eec7f359cf989835d4d
gastonamengual <gastonamengual@icloud.com>
2025-03-05 19:47:04 +0100

    Refactor Render Deployment Logic

--- render_deployment.py
+++ render_deployment.py
@@ -1,41 +1,37 @@
-from dataclasses import dataclass
 import requests  # type: ignore
 from gamr_backend_api_service.settings import Settings
 
 
-@dataclass
-class RenderDeployment:
+def get_service_id():
+    url = "https://api.render.com/v1/services?includePreviews=true&limit=20"
 
-    def get_service_id(self):
-        url = "https://api.render.com/v1/services?includePreviews=true&limit=20"
+    headers = {
+        "accept": "application/json",
+        "authorization": f"Bearer {Settings.RENDER_API_TOKEN}",
+    }
 
-        headers = {
-            "accept": "application/json",
-            "authorization": f"Bearer {Settings.RENDER_API_TOKEN}",
-        }
-
-        response = requests.get(url, headers=headers)
-        return response
-
-    def deploy_service(self, service_id: str):
-        url = f"https://api.render.com/v1/services/{service_id}/deploys"
-
-        payload = {"clearCache": "do_not_clear"}
-        headers = {
-            "accept": "application/json",
-            "content-type": "application/json",
-            "authorization": f"Bearer {Settings.RENDER_API_TOKEN}",
-        }
-
-        response = requests.post(url, json=payload, headers=headers)
-
-        return response
+    response = requests.get(url, headers=headers)
+    return response
 
 
-render_deployment = RenderDeployment()
-response = render_deployment.get_service_id()
+def deploy_service(service_id: str):
+    url = f"https://api.render.com/v1/services/{service_id}/deploys"
+
+    payload = {"clearCache": "do_not_clear"}
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "authorization": f"Bearer {Settings.RENDER_API_TOKEN}",
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    return response
+
+
+response = get_service_id()
 service_id = response.json()[0]["service"]["id"]
 print(f"Retrieved service with id: {service_id}")
-response = render_deployment.deploy_service(service_id)
+response = deploy_service(service_id)
 if response.ok:
     print("Render deployment triggered successfully")
